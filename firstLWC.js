20f3b523f54fc9745b54b2b7846bbac59bb74fc7
Ravi299 <59054902+Ravi299@users.noreply.github.com>
2021-10-02 22:01:31 +0530

    Add files via upload

---
+++ firstLWC.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstLWC extends LightningElement {
+    firstName = '';
+    lastName = '';
+    areDetailsVisible = false;
+
+    handleChange(event) {
+        const field = event.target.name;
+        if (field === 'firstName') {
+            this.firstName = event.target.value;
+        } else if (field === 'lastName') {
+            this.lastName = event.target.value;
+        }
+        this.areDetailsVisible = event.target.checked;
+    }
+
+    get upperCasedFullName() {
+        return `${this.firstName} ${this.lastName}`.toUpperCase();
+    }
+}
